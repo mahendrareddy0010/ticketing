957c32c50bf64432ec0a05ffea6799bb446c8b6a
Mahendra <mahendrareddyyarramreddy@gmail.com>
2023-11-05 13:12:23 +0530

    added env variables

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { randomBytes } from "crypto";
 import { app } from "./app";
 import { natsWrapper } from "./nats-wrapper";
 require("dotenv").config();
@@ -7,18 +8,26 @@ const start = async () => {
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY : env variable is not defined");
   }
+  if (!process.env.TICKETS_SRV_MONGO_URI) {
+    throw new Error("TICKETS_SRV_MONGO_URI : env varianle is not defined");
+  }
+  if (!process.env.CLUSTER_ID || !process.env.NATS_URL) {
+    throw new Error("some env varaibles are not defined");
+  }
   try {
-    await natsWrapper.connect("ticketing", "hfkjhkjsdf", "http://0.0.0.0:4222");
-    natsWrapper.client.on('close', () => {
-      console.log('NATS connection closed')
-      process.exit();
-    })
-    process.on('SIGINT', () => natsWrapper.client.close());
-    process.on('SIGTERM', () => natsWrapper.client.close());
-
-    await mongoose.connect(
-      "mongodb+srv://mahendrareddyyarramreddy:simple123@cluster0.1vhaq24.mongodb.net/?retryWrites=true&w=majority"
+    await natsWrapper.connect(
+      process.env.CLUSTER_ID,
+      randomBytes(4).toString("hex"),
+      process.env.NATS_URL
     );
+    natsWrapper.client.on("close", () => {
+      console.log("NATS connection closed");
+      process.exit();
+    });
+    process.on("SIGINT", () => natsWrapper.client.close());
+    process.on("SIGTERM", () => natsWrapper.client.close());
+
+    await mongoose.connect(process.env.TICKETS_SRV_MONGO_URI);
     console.log("Connected to DB");
   } catch (err) {
     console.log(err);

d431501371624a922dbfecce54f03f1e2016985e
Mahendra <mahendrareddyyarramreddy@gmail.com>
2023-11-10 11:57:59 +0530

    added sample msg

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,6 +3,7 @@ import { app } from "./app";
 require('dotenv').config()
 
 const start = async () => {
+  console.log('Staring up...')
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY : env variable is not defined");
   }

d6fe137a677a2aff960f7a8ecfd606a55d6d2ea2
Mahendra <mahendrareddyyarramreddy@gmail.com>
2023-11-10 14:14:36 +0530

    sample

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -8,6 +8,7 @@ import { ExpirationCompleteListener } from "./events/listeners/expiration-comple
 require("dotenv").config();
 
 const start = async () => {
+  console.log("Satrting the orders service....");
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY : env variable is not defined");
   }
